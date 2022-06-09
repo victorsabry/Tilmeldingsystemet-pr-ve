96fabcd196ee5cfc419362884ce64ca52e4ca02d
Victor Sabry <victorsabry@hotmail.com>
2022-06-09 10:34:55 +0200

    Add project files.

---
+++ Tilmeldingsystemet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tilmeldingsystemet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
